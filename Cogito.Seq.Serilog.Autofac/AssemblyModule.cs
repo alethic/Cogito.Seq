87eab2ce2e16802f3cabf11b2bc36ad43e25dba0
Jerome Haltom <jhaltom@alethic.solutions>
2020-11-24 20:42:38 -0600

    Add Seq integration.

---
+++ Cogito.Seq.Serilog.Autofac/AssemblyModule.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+
+using Cogito.Autofac;
+using Cogito.Serilog;
+
+namespace Cogito.Seq.Serilog.Autofac
+{
+
+    public class AssemblyModule : ModuleBase
+    {
+
+        /// <summary>
+        /// Invoked to load the module.
+        /// </summary>
+        /// <param name="builder"></param>
+        protected override void Register(ContainerBuilder builder)
+        {
+            builder.RegisterFromAttributes(typeof(AssemblyModule).Assembly);
+            builder.RegisterType<SeqLoggerConfigurator>().As<ILoggerConfigurator>();
+        }
+
+    }
+
+}
---
+++ Cogito.Seq.Serilog/SeqLoggerConfigurator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Cogito.Serilog;
+
+using Microsoft.Extensions.Options;
+
+using Serilog;
+
+namespace Cogito.Seq.Serilog
+{
+
+    /// <summary>
+    /// Configures Serilog to send logs to Seq.
+    /// </summary>
+    public class SeqLoggerConfigurator : ILoggerConfigurator
+    {
+
+        readonly IOptions<SeqOptions> options;
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="options"></param>
+        public SeqLoggerConfigurator(IOptions<SeqOptions> options)
+        {
+            this.options = options ?? throw new ArgumentNullException(nameof(options));
+        }
+
+        public LoggerConfiguration Apply(LoggerConfiguration configuration)
+        {
+            if (options.Value != null && !string.IsNullOrEmpty(options.Value.ServerUrl) && !string.IsNullOrEmpty(options.Value.ApiKey))
+                return configuration.WriteTo.Seq(options.Value.ServerUrl, apiKey: options.Value.ApiKey);
+
+            return configuration;
+        }
+
+    }
+
+}
