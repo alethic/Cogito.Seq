3ea65af347345b53bbd72998a192e981474aed5b
Jerome Haltom <jhaltom@alethic.solutions>
2020-11-24 20:38:02 -0600

    Initial import.

---
+++ Cogito.Seq.Autofac/AssemblyModule.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+
+using Cogito.Autofac;
+using Cogito.Extensions.Options.Configuration.Autofac;
+
+namespace Cogito.Seq.Autofac
+{
+
+    public class AssemblyModule : ModuleBase
+    {
+
+        /// <summary>
+        /// Invoked to load the module.
+        /// </summary>
+        /// <param name="builder"></param>
+        protected override void Register(ContainerBuilder builder)
+        {
+            builder.RegisterFromAttributes(typeof(AssemblyModule).Assembly);
+            builder.Configure<SeqOptions>("Seq");
+        }
+
+    }
+
+}
---
+++ Cogito.Seq/SeqOptions.cs
@@ -0,0 +1,22 @@
+﻿namespace Cogito.Seq
+{
+
+    /// <summary>
+    /// Provides configuration options for Seq.
+    /// </summary>
+    public class SeqOptions
+    {
+
+        /// <summary>
+        /// Gets the URL of the Seq server.
+        /// </summary>
+        public string ServerUrl { get; set; }
+
+        /// <summary>
+        /// Gets the API key of the Seq server.
+        /// </summary>
+        public string ApiKey { get; set; }
+
+    }
+
+}
